cee0550ee8bf38380f08ec4e617b2df42d8760c3
Knagrecha <32966638+Knagrecha@users.noreply.github.com>
2018-08-04 17:49:45 -0700

    Create __init__.py

---
+++ gym-building/__init__.py
@@ -0,0 +1,26 @@
+import logging
+
+from gym.envs.registration import register
+
+
+
+logger = logging.getLogger(__name__)
+
+
+
+register(
+
+    id='g=Building-v0',
+
+    entry_point='gym-building.envs:BuildingEnv',
+
+    timestep_limit=1000,
+
+    reward_threshold=1.0,
+
+    nondeterministic = True,
+
+)
+
+
+
